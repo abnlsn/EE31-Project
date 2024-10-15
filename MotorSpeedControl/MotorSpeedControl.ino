bacfbbb08ce5335afc9b5f3aa31f96680430f011
Abe Nelson <29802261+abnlsn@users.noreply.github.com>
2024-10-15 16:33:16 -0400

    Debug script for graphing speed vs duty cycle

--- MotorSpeedControl/MotorSpeedControl.ino
+++ MotorSpeedControl/MotorSpeedControl.ino
@@ -1,25 +1,63 @@
+#include "EE31_motor_drive.h"
+
 int left_motor_encoder = 3;
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(left_motor_encoder, INPUT);
+  statemachine_setup();
+  Serial.begin(9600);
+  pinMode(left_motor_encoder, INPUT);
 
-    Serial.println("HI");
+  Serial.println("Duty Cycle,ms per rev");
 
-    attachInterrupt(digitalPinToInterrupt(left_motor_encoder), rotation, RISING);
+  attachInterrupt(digitalPinToInterrupt(left_motor_encoder), rotation, RISING);
 }
 
-void loop() {
-}
+
 
 int last_millis = 0;
+
+int count = 0;
+int current_duty = 20;
+float sum = 0;
+
+const int NUM_ROTATIONS = 50;
 
 void rotation() {
   int current = millis();
   int diff_time = current - last_millis;
-  float mps = 0.5 * 3.1415 / diff_time;
-  Serial.println(mps);
+
   last_millis = current;
 
+  if (last_millis == 0) return;
 
+  float mps = 0.5 * 3.1415 / diff_time;
+  // Serial.println(mps);
+
+  sum += diff_time;
+
+  count++;
+  if (count > NUM_ROTATIONS) {
+    count = 0;
+    sum = sum / NUM_ROTATIONS;
+    Serial.print(current_duty);
+    Serial.print(",");
+    Serial.print(sum);
+    Serial.println(",");
+
+    // reset
+    sum = 0;
+    current_duty += 5;
+    left_fwd(current_duty);
+  }
 }
+
+void loop() {
+  if (current_duty > 255) {
+    detachInterrupt(digitalPinToInterrupt(left_motor_encoder));
+    Serial.println("Deactivated");
+    left_fwd(0);
+    while (true);
+  }
+  delay(100);
+}
+
